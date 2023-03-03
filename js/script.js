3e75d1df4758bca02e8bb1b87fd32be3bdc81fb5
miketochinskiy <120083573+miketochinskiy@users.noreply.github.com>
2023-03-03 11:01:05 +0000

    Logo javascript creation

--- js/script.js
+++ js/script.js
@@ -2,7 +2,29 @@ console.log('JS Connected');
 
 gsap.from("#name", {
     scale: 5,
-    duration: 2,
-    rotate: 180,
+    duration: 3,
+//    rotate: 180,
     ease: "bounce",
+})
+
+gsap.from("#box", {
+    duration: 1.5,
+    scale: .001,
+    rotate: 40,
+    x: 1000,
+    y: 1000,
+})
+
+gsap.from("#M", {
+    duration: 2,
+    scale: .001,
+    x: 2000,
+    y: 1000,
+})
+
+gsap.from("#T", {
+    duration: 3,
+    scale: .001,
+    x: 1500,
+    y: 1000,
 })
